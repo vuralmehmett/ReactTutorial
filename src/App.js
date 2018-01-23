6e5120673d0734e58aaca33073200f7bc171b13f
vuralmehmett <mehmetvrl@hotmail.com>
2018-01-24 00:47:37 +0300

    add istanbul library and set to coverage on test files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Welcome to React App</h1>
     </header>
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
